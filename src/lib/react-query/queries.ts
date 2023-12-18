0219461afdb1a2db57199d20bd5ded0978fa13ad
JATIN KUMAR VERMA <82764118+jatinkumarverma@users.noreply.github.com>
2023-12-18 13:44:07 +0530

    Error fixed

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -57,20 +57,19 @@ export const useSignOutAccount = () => {
 
 export const useGetPosts = () => {
   return useInfiniteQuery({
-    queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-    queryFn: getInfinitePosts as any,
-    getNextPageParam: (lastPage: any) => {
-      // If there's no data, there are no more pages.
-      if (lastPage && lastPage.documents.length === 0) {
-        return null;
-      }
+      queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+      queryFn: ({ pageParam }) => getInfinitePosts({ pageParam }),
+      getNextPageParam: (lastPage: any) => {
+          if (!lastPage || !lastPage.documents || lastPage.documents.length === 0) {
+              return null
+          }
 
-      // Use the $id of the last document as the cursor.
-      const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
-      return lastId;
-    },
-  });
-};
+          const lastId = lastPage.documents[lastPage.documents.length - 1].$id
+          return lastId
+      },
+      initialPageParam: 0,
+  })
+}
 
 export const useSearchPosts = (searchTerm: string) => {
   return useQuery({
